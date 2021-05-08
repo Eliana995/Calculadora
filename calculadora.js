2e958e0d09524d2085d478965c8f3092cc1a34dd
Eliana995 <eliana.ag.95@gmail.com>
2021-05-07 22:26:51 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+const sumar = require('./calculadora/sumar');
+const restar = require('./calculadora/restar');
+const multiplicar = require('./calculadora/multiplicar');
+const dividir = require('./calculadora/dividir');
+
+
+console.log(dividir(40, 5));
